fb8681c2571e38d0ebc689df8b7c4abda26d527f
Alina <gav621@mail.ru>
2022-11-12 16:22:21 +0300

    Lesson 01

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. С клавиатуры вводится целое число. Вывести квадрат числа
+
+int a;
+System.Console.Write("Введите число: ");
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+int b=a*a;
+System.Console.WriteLine(a+"^2="+b);
+
+System.Console.WriteLine($"{a}^2={b}");
---
+++ 09/Program.cs
@@ -0,0 +1,9 @@
+﻿// 9. Вывести на экран четные числа от 1 до N
+
+int N=Convert.ToInt32(Console.ReadLine());
+int i=2;
+while(i<=N)
+{
+    System.Console.Write($"{i} ");
+    i=i+2;
+}
