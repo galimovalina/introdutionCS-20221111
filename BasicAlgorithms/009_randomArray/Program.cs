3fc6da602877e33694ec6fdae222af70bed23bc4
Alina <gav621@mail.ru>
2022-11-23 22:37:52 +0300

    lesson 4.1

--- BasicAlgorithms/009_randomArray/Program.cs
+++ BasicAlgorithms/009_randomArray/Program.cs
@@ -2,12 +2,12 @@
 
 
 
-int[] RandomIntArray(int size)
+int[] RandomIntArray(int size, int min=0, int max=100)
 {
     int[] a=new int[size];
     Random random=new Random();
     for(int i=0;i<size;i++)
-        a[i]=random.Next(0,100);
+        a[i]=random.Next(min,max+1);
     return a;
 }
 
---
+++ BasicAlgorithms/010_linear_search_array/Program.cs
@@ -0,0 +1,34 @@
+﻿// Линейный поиск в массиве
+
+int[] RandomIntArray(int size = 10)
+{
+    int[] a = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+        a[i] = random.Next(0, 100);
+    return a;
+}
+
+void Print(int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+        System.Console.Write($"{a[i],5}");
+}
+
+
+int LinearSearch(int[] m, int find)
+{
+    int i = 0;
+    while (i < m.Length && m[i] != find) i++;
+    if (i == m.Length) return -1;
+    else return i;
+}
+
+int[] m = RandomIntArray();
+int find = 20;
+m[4] = find;
+Print(m);
+System.Console.WriteLine();
+int l=LinearSearch(m,find);
+if (l==-1) System.Console.WriteLine($"Element {find} not found");
+    else System.Console.WriteLine($"Element {find} found at {l}");
