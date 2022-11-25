da61caf4468cf8cf342b05fb3b8c18a76a65c826
Alina <gav621@mail.ru>
2022-11-25 21:37:18 +0300

    lesson 5

---
+++ BasicAlgorithms/011_array_reverse/Program.cs
@@ -0,0 +1,38 @@
+﻿// Переворот массива
+
+int[] RandomIntArray(int size, int min = 0, int max = 100)
+{
+    int[] a = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+        a[i] = random.Next(min, max + 1);
+    return a;
+}
+
+void Print(int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+        System.Console.Write($"{a[i],5}");
+}
+
+void Swap(ref int a, ref int b)
+{
+    int t=a;
+    a=b;
+    b=t;
+}
+
+void Reverse(int[] a)
+{
+    for(int i=0; i<a.Length/2;i++)
+    {
+        Swap(ref a[i], ref a[a.Length-1-i]);
+    }
+}
+
+
+int[] m = RandomIntArray(10, 1, 10);
+Print(m);
+System.Console.WriteLine();
+Reverse(m);
+Print(m);
---
+++ BasicAlgorithms/012_binary_search/Program.cs
@@ -0,0 +1,39 @@
+﻿// Бинарный поиск
+
+int[] RandomIntArray(int size, int min = 0, int max = 100)
+{
+    int[] a = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < size; i++)
+        a[i] = random.Next(min, max + 1);
+    return a;
+}
+
+void Print(int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+        System.Console.Write($"{a[i],5}");
+}
+
+int BinSearch(int[] a, int find)
+{
+    int i;;
+    int left=0;
+    int right=a.Length-1;
+    i=left+(right-left)/2;
+    while(a[i]!=find && left<right)
+    {
+        if (find>a[i])
+        left=i;
+        else right=i;
+        i=left+(right-left)/2;
+    }
+    if (a[i]!=find) return -1;
+    else
+    return i;
+}
+
+int[] m = RandomIntArray(10, 1, 10);
+Print(m);
+m[4]=2;
+System.Console.WriteLine("\n"+BinSearch(m,2));
