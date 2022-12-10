f74b80b9c13ac506b6ab70028d5c5dfc2cec536f
Alina <gav621@mail.ru>
2022-12-10 11:27:44 +0300

    homework 8

--- 84/Program.cs
+++ 84/Program.cs
@@ -1,9 +1,26 @@
 ﻿// 84. Определить являются ли введенные с клавиатуры символы правильно записью целого числа. Вычислить сумму цифр введенного числа
 
-string s = Console.ReadLine();
-int sum = 0;
-for (int i = 0; i < s.Length; i++)
-    if (char.IsDigit(s[i])) sum += s[i] - 48;
-else Console.WriteLine("Это не является верной записью целого");
-System.Console.WriteLine($"Сумма цифр введенного числа: {sum}");
+int SumDigits(int n)
+{
+    int sum=0;
+    n=Math.Abs(n);
+    while(n!=0)
+    {
+        sum=sum+n%10;
+        n=n/10;
+    }
+    return sum;
+}
+
+string s=Console.ReadLine();
+int n;
+bool flag=int.TryParse(s,out n);
+if (flag)
+{
+       System.Console.WriteLine($"{n} {SumDigits(n)}");
+
+} 
+else System.Console.WriteLine("Wrong number");
+
+
 
