151fb9baf96c6e608c3d144ff43542bc6ca63a0d
Alina <gav621@mail.ru>
2022-12-11 20:18:49 +0300

    lesson 9

---
+++ BasicAlgorithms/023_Delegates,Lambda,LINQ/Program.cs
@@ -0,0 +1,99 @@
+﻿// Подсчитать сумму цифр, встречающихся в строке
+
+// string s="12345";
+// int sum=s.ToCharArray().Cast<int>().Sum();
+// System.Console.WriteLine();
+
+//Делегаты
+
+// int CountDigits(string s)
+// {
+//     int count=0;
+//     foreach (char c in s)
+//     if (char.IsDigit(c))
+//     count ++;
+//     return count;
+// }
+
+// int CountLatin(string s)
+// {
+//     int count=0;
+//     foreach (char c in s)
+//     if (c>='A' && c<='Z' || c>='a' && c<='z')
+//     //if (char.IsLetter(c))
+//     count ++;
+//     return count;
+// }
+
+// int CountRus(string s)
+// {
+//     int count=0;
+//     foreach (char c in s)
+//     if (c>='А' && c<='Я' || c>='а' && c<='я' || c=='Ё' || c=='ё')
+//         count ++;
+//     return count;
+// }
+
+using System.Linq;
+delegate bool IsChar(char c); //сигнатура bool(char)
+
+class Program
+{
+    static int CountChars(string s, IsChar IsChar)
+{
+    int count=0;
+    foreach (char c in s)
+    if (IsChar(c))
+       count ++;
+    return count;
+}
+
+static bool IsLatin(char c)
+{
+    return (c>='A' && c<='Z' || c>='a' && c<='z');
+}
+
+static bool IsRus(char c)
+{
+return ((c>='А' && c<='Я' || c>='а' && c<='я' || c=='Ё' || c=='ё'));
+}
+
+static void Main()
+{
+    // Action click;
+    // click =new Action(выделение);
+    // click=new Action(заливка;)
+    //Func
+    string s="Эта строка содержит latinskie буквы и цифры 12345";
+    //System.Console.WriteLine(CountChars(s,IsLatin));
+    System.Console.WriteLine(CountChars(s,delegate(char c)//анонимный делегат
+    {
+        return (c>='A' && c<='Z' || c>='a' && c<='z');
+    }
+    ));
+    System.Console.WriteLine(CountChars(s,IsRus));
+}
+}
+
+
+//Лямбда выражения
+
+System.Console.WriteLine(CountChars(s,(c)=>//лямбда-выражение многострочное
+    {
+        return (c>='A' && c<='Z' || c>='a' && c<='z');
+    }
+));
+
+System.Console.WriteLine(CountChars(s,(c)=>c>='A' && c<='Z' || c>='a' && c<='z'));//лямбда-выражение однострочное
+
+//LINQ
+
+static void Main()
+{
+    string s="Эта строка содержит latinskie буквы и цифры 12345";
+    var queary=(from c in s where (c>='A' && c<='Z' || c>='a' && c<='z') select c).Count();
+    System.Console.WriteLine(queary);
+
+    System.Console.WriteLine(CountChars(s,IsLatin));
+    System.Console.WriteLine(CountChars(s,IsRus));
+}
---
+++ BasicAlgorithms/024_Частотный словарь/Program.cs
@@ -0,0 +1,18 @@
+﻿//Частотный словарь
+//Составить частотный словарь элементов одномерного массива состоящих из чисел от 0 до 99.
+
+int[] data = { 1, 3, 4, 8, 3, 5, 8 };
+int[] freqArray = new int[100];
+foreach (int element in data)
+    freqArray[element]++;
+for (int i = 0; i < freqArray.Length; i++)
+    if (freqArray[i] != 0)
+        System.Console.WriteLine($"{i} {freqArray[i]}");
+
+
+Dictionary<int, int> freqDic = new Dictionary<int, int>();
+foreach (int element in data)
+    if (freqDic.ContainsKey(element)) freqDic[element]++;
+    else freqDic.Add(element, 1);
+foreach (var el in freqDic)
+    System.Console.WriteLine($"{el.Key} {el.Value}");
