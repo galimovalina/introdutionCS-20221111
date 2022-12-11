13031ce03200d809b2e8a06180c146837390a39e
Alina <gav621@mail.ru>
2022-12-11 23:11:14 +0300

    homework 9

---
+++ 90/Program.cs
@@ -0,0 +1,45 @@
+﻿// 90. Есть два массива info и data.
+// Массив data состоит из нулей и единиц хранящий числа в двоичном представлении. 
+//Числа идут друг за другом без разделителей.
+// Массив info состоит из чисел, которые представляют количество бит чисел из массива data.
+// Составить массив десятичных представлений чисел массива data с учётом информации из массива info.
+// Пример:
+// входные данные:
+// data = {0, 1, 1, 1, 1, 0, 0, 0, 1 }
+// info = {2, 3, 3, 1 }
+
+// выходные данные:
+// 1, 7, 0, 1
+
+int[] RandomDataArray(int size, int min=0, int max=1)
+{
+    int[] a=new int[size];
+    Random random=new Random();
+    for(int i=0;i<size;i++)
+        a[i]=random.Next(min,max+1);
+    return a;
+}
+
+int[] RandomInfoArray(int size, int min=0, int max=9)
+{
+    int[] a=new int[size];
+    Random random=new Random();
+    for(int i=0;i<size;i++)
+        a[i]=random.Next(min,max+1);
+    return a;
+}
+
+void Print(int[] a)
+{
+    for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i], 5}");
+}
+
+int size=5;
+int[] data=RandomDataArray(size);
+Print(data);
+System.Console.WriteLine();
+int[] info=RandomInfoArray(5);
+Print(info);
+
+//int [] info=new int[];
---
+++ 93/Program.cs
@@ -0,0 +1,41 @@
+﻿// 93. Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+int[,] Random2DArray(int n, int m, int min = 1, int max = 9)
+{
+    int[,] a = new int[n, m];
+    Random random = new Random();
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            a[i, j] = random.Next(min, max + 1);
+    return a;
+}
+
+void Print2DArray(int[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+            System.Console.Write($"{a[i, j],4}");
+        System.Console.WriteLine();
+    }
+}
+
+int[,] PowArrays(int[,] a, int[,] b)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    for ( i = 0; i < b.GetLength(0); i++)
+        for (int j = 0; j < a.GetLength(1); j++)
+        for ( j = 0; j < b.GetLength(1); j++)
+        a[i,j]=a[i,j]*b[i,j];
+            
+    return a;
+}
+
+int[,] a = Random2DArray(4, 4);
+int[,] b = Random2DArray(4, 4);
+Print2DArray(a);
+System.Console.WriteLine();
+Print2DArray(b);
+PowArrays(a,b);
+System.Console.WriteLine();
+Print2DArray(a);
